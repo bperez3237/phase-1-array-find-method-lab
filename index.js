9e74ef7e9b1d5825a15fab478430e07f7f038396
Brian <b.perez3237@gmail.com>
2022-02-10 19:00:04 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function superbowlWin(record) {
+    let x = record.find(isWin);
+    if (!( x === undefined)) {
+        return x.year;
+    }
+
+    return undefined;
+
+}
+
+function isWin(game, index, array) {
+    let result = undefined;
+
+    if (game.result === 'W') {
+        //console.log('if state');
+        //console.log(game.year);
+        result = game;
+        return result;
+    };
+
+}
